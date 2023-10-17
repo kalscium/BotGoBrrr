472f48dcd36a5165eeff3236eb8a0b0c4801b4a5
GreenChild04 <106739622+GreenChild04@users.noreply.github.com>
2023-10-17 21:11:56 +1100

    Changed to &'static mut instead of &mut for bot

--- bot_go_brr/src/bot.rs
+++ bot_go_brr/src/bot.rs
@@ -54,13 +54,11 @@ impl Robot for Bot {
         }
     }
 
-    fn initialize(&mut self, _ctx: Context) {
+    fn initialize(&'static mut self, _ctx: Context) {
         // Do any extra initialization here.
-
-        // Init controller screen data
     }
 
-    fn autonomous(&mut self, _ctx: Context) {
+    fn autonomous(&'static mut self, _ctx: Context) {
         let mut l = Loop::new(Duration::from_millis(Config::TICK_SPEED));
         let mut tick: usize = 0;
         let algor =
@@ -90,7 +88,7 @@ impl Robot for Bot {
         }
     }
 
-    fn opcontrol(&mut self, ctx: Context) {
+    fn opcontrol(&'static mut self, ctx: Context) {
         // This loop construct makes sure the drive is updated every 100 milliseconds.
         let mut l = Loop::new(Duration::from_millis(Config::TICK_SPEED));
         let mut tick: usize = 0;
@@ -125,13 +123,13 @@ impl Robot for Bot {
         }
     }
 
-    fn disabled(&mut self, _ctx: Context) {
+    fn disabled(&'static mut self, _ctx: Context) {
         // This runs when the robot is in disabled mode.
     }
 }
 
 impl Bot {
-    pub fn driver(&mut self) -> DriveArg{
+    pub fn driver(&'static mut self) -> DriveArg{
         let packet = controller::Packet::new(&self.controller);
         if let controller::Packet::Disconnected = packet {
             DriveArg::Forward(ButtonArg::Null, false)
