5c8652cf1cb74709817432a75d40af1bab702cca
GreenChild04 <greenchild04@protonmail.com>
2023-12-04 00:10:29 +1100

    updated to the latest safe-vex version

--- bot_go_brr/src/bot.rs
+++ bot_go_brr/src/bot.rs
@@ -1,72 +1,66 @@
-use safe_vex::{bot::Bot, context::Context, Mutex};
+use safe_vex::{bot::Bot, context::Context};
 use crate::{drive::{Drive, DriveState, DriveArg}, auto::Auto, config::Config};
 #[cfg(debug_assertions)]
 use crate::record::Record;
 
 pub struct Robot {
-    drive: Mutex<Drive>,
-    autonomous: Mutex<Auto>,
+    drive: Drive,
+    autonomous: Auto,
     /// **Used buttons:** `a` & `x`
-    button_before: Mutex<(bool, bool)>,
+    button_before: (bool, bool),
     #[cfg(debug_assertions)]
-    record: Mutex<Record>,
+    record: Record,
 }
 
 impl<'a> Bot<'a> for Robot {
     #[inline]
-    fn new(ctx: &'a Context) -> Self {
+    fn new(context: &'a Context) -> Self {
         Self {
-            drive: Mutex::new(Drive::new(ctx)),
-            autonomous: Mutex::new(Config::AUTO_COMPETITION),
-            button_before: Mutex::new((false, false)),
+            drive: Drive::new(context),
+            autonomous: Config::AUTO_COMPETITION,
+            button_before: (false, false),
             #[cfg(debug_assertions)]
-            record: Mutex::new(Record::new()),
+            record: Record::new(),
         }
     }
 
     #[inline]
-    fn autonomous(&'a self, context: &'a Mutex<Context>) {
+    fn autonomous(&'a mut self, context: &'a Context) {
         // Get the autonomous argument
-        if let Some(x) = self.autonomous.lock().next() {
-            self.drive.lock().run(&context.lock(), x);
+        if let Some(x) = self.autonomous.next() {
+            self.drive.run(&context, x);
         }
     }
 
     #[inline]
-    fn opcontrol(&'a self, context: &'a Mutex<Context>) {
+    fn opcontrol(&'a mut self, context: &'a Context) {
         // Get the current drive-train state
-        let (drive_state, flush_logs) = {
-            // Get the controls from the controller safely
-            let mut context = context.lock();
-            let mut controller = context.controller();
+        let drive_state = DriveState::new(&DriveArg::new(
+            context.controller().left_stick(),
+            context.controller().right_stick(),
+            context.controller().l2(),
+            context.controller().r2(),
+            context.controller().up() || context.controller().x(),
+            context.controller().down() || context.controller().b(),
+        ));
 
-            // flush the recorded autonomous if required
-            #[cfg(debug_assertions)]
-            if controller.a() && !self.button_before.lock().0 {
-                self.button_before.lock().0 = true;
-                self.record.lock().flush();
-            } else { self.button_before.lock().1 = false };
-
-            (DriveState::new(&DriveArg::new(
-                controller.left_stick(),
-                controller.right_stick(),
-                controller.l2(),
-                controller.r2(),
-                controller.up() || controller.x(),
-                controller.down() || controller.b(),
-            )), controller.y())
-        };
+        // flush the recorded autonomous if required
+        #[cfg(debug_assertions)]
+        if context.controller().a() && !self.button_before.0 {
+            self.button_before.0 = true;
+            self.record.flush();
+        } else { self.button_before.0 = false };
 
         // flush logs if required
-        if flush_logs && !self.button_before.lock().1 {
-            self.button_before.lock().1 = true;
-            context.lock().flush_logs();
-        } else { self.button_before.lock().1 = false };
+        if context.controller().y() && !self.button_before.1 {
+            self.button_before.1 = true;
+            context.flush_logs();
+        } else { self.button_before.1 = false };
 
         // Record the drive-state if not release
         #[cfg(debug_assertions)]
-        self.record.lock().record(drive_state);
+        self.record.record(drive_state);
 
-        self.drive.lock().run(&context.lock(), drive_state);
+        self.drive.run(&context, drive_state);
     }
 }
