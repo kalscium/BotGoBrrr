5bfc4fd112d4469555b8b55fa75adbc7a5b684bd
GreenChild04 <greenchild04@protonmail.com>
2024-05-24 08:38:13 +1000

    feature bugfixes: added the feature tag to the cfg attr to make it actually work

--- bot_go_brr/src/bot.rs
+++ bot_go_brr/src/bot.rs
@@ -1,12 +1,12 @@
 use alloc::{boxed::Box, vec::Vec};
 use safe_vex::{bot::Bot, context::Context, maybe::Maybe, motor::Motor, port::PortManager, vex_rt::peripherals::Peripherals};
 use crate::{append_slice, bytecode::{execute, ByteCode}, config, controls, drive_train::DriveTrain};
-#[cfg(record)]
+#[cfg(feature = "record")]
 use crate::record::Record;
 
 /// The robot
 pub struct Robot {
-    #[cfg(record)]
+    #[cfg(feature = "record")]
     record: Record,
 
     /// The drive-train of the robot
@@ -27,16 +27,16 @@ impl Bot for Robot {
         let drive_train = DriveTrain::new(port_manager);
 
         Self {
-            #[cfg(record)]
+            #[cfg(feature = "record")]
             record: Record::new(),
             
             drive_train,
             belt: Maybe::new(Box::new(|| unsafe { Motor::new(config::drive::BELT.port, config::drive::GEAR_RATIO, config::drive::UNIT, config::drive::BELT.reverse) }.ok())),
 
             // load the autonomous bytecode
-            #[cfg(full_autonomous)]
+            #[cfg(feature = "full-autonomous")]
             bytecode: config::autonomous::FULL_AUTO.to_vec(),
-            #[cfg(not(full_autonomous))]
+            #[cfg(not(feature = "full-autonomous"))]
             bytecode: config::autonomous::COMP_AUTO.to_vec(),
         }
     }
@@ -47,7 +47,7 @@ impl Bot for Robot {
         let drive_inst = controls::gen_drive_inst(&context.controller);
 
         // get belt bytecode inst and push it to the bytecode
-        let belt_inst = match (context.controller.x, context.controller.y) {
+        let belt_inst = match (context.controller.x, context.controller.b) {
             (true, _) => ByteCode::Belt { voltage: config::drive::BELT_VOLTAGE },
             (_, true) => ByteCode::Belt { voltage: -config::drive::BELT_VOLTAGE },
             (_, _) => ByteCode::Belt { voltage: 0 },
@@ -61,10 +61,16 @@ impl Bot for Robot {
         execute(&mut self.bytecode, &mut self.drive_train, &mut self.belt);
 
         // append to record
-        #[cfg(record)]
+        #[cfg(feature = "record")]
         {
             self.record.append(&drive_inst);
             self.record.append(&[belt_inst]);
+        }
+
+        // check if record is flushed
+        #[cfg(feature = "record")]
+        if context.controller.y {
+            self.record.flush();
         }
         
         false
--- bot_go_brr/src/lib.rs
+++ bot_go_brr/src/lib.rs
@@ -8,7 +8,7 @@ pub mod drive_train;
 pub mod config;
 pub mod controls;
 pub mod bytecode;
-#[cfg(record)]
+#[cfg(feature = "record")]
 pub mod record;
 
 #[inline]
