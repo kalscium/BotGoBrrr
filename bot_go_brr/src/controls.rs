6f538c60bbfbaac93dd2a8f62b1a0030434035ed
kalscium <kalscium@protonmail.com>
2024-09-10 09:58:50 +1000

    arcade controls: added basic arcade controls and also removed reverse driving as it's confusing

--- bot_go_brr/src/controls.rs
+++ bot_go_brr/src/controls.rs
@@ -6,32 +6,18 @@ use crate::{bytecode::ByteCode, config, powf};
 pub fn gen_drive_inst(controller: &Controller) -> [ByteCode; 2]  {
     // get joystick values and reverse values
     let j1 = &controller.left_stick;
-    let reversed = controller.l1;
 
     // get the calculated voltages from the absolute x & y of the joystick
     let j1xv = (1024.0 * powf(config::DMN as f64, j1.x.abs() as f64) - 1024.0)
         * if controller.l2 { config::drive::PRECISE_MULTIPLIER as f64 } else { 1.0 }; // precise turning
     let j1yv = (1024.0 * powf(config::DMN as f64, j1.y.abs() as f64) - 1024.0)
         * if controller.l2 { config::drive::PRECISE_MULTIPLIER as f64 } else { 1.0 }; // precise turning
+
     // left drive & right drive
-    let (ldr, rdr) = match (j1.x_larger(), j1.x.is_positive(), j1.y.is_positive(), reversed) {
-        // move forward
-        (false, _, true, false) => (j1yv, j1yv), // normal
-        (false, _, true, true) => (-j1yv, -j1yv), // while reversed (driving backwards)
-        // move backwards
-        (false, _, false, false) => (-j1yv, -j1yv), // normal
-        (false, _, false, true) => (j1yv, j1yv), // while reversed (driving backwards)
-
-        _ => {
-            let voltage = j1xv * config::drive::TURN_SPEED as f64;
-
-            if j1.x.is_positive() {
-                (voltage, -voltage)
-            } else {
-                (-voltage, voltage)
-            }
-        },
-    };
+    let (ldr, rdr) = (
+        (j1yv + j1xv).clamp(-12000.0, 12000.0),
+        (j1yv - j1xv).clamp(-12000.0, 12000.0),
+    );
 
     [
         ByteCode::LeftDrive { voltage: ldr as i32 },
