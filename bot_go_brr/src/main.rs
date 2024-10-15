5218a5ee8936d55059f1a9847bee5bf16ab926a2
kalscium <kalscium@protonmail.com>
2024-10-15 22:03:13 +1100

    safe vex: entry macro: updated safe-vex to use the new and improved entry macro

--- bot_go_brr/src/main.rs
+++ bot_go_brr/src/main.rs
@@ -2,7 +2,7 @@
 #![no_main]
 
 safe_vex::entry! {
-    initialize: (bot_go_brr::initialize::initialize);
-    opcontrol: (bot_go_brr::opcontrol::opcontrol);
-    autonomous: (bot_go_brr::autonomous::autonomous);
+    initialize => bot_go_brr::initialize::initialize();
+    opcontrol => bot_go_brr::opcontrol::opcontrol();
+    autonomous => bot_go_brr::autonomous::autonomous();
 }
