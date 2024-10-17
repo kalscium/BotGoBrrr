b0f2f42318029c926546aa4d30985e979c61205e
kalscium <kalscium@protonmail.com>
2024-10-17 18:38:34 +1100

    solenoid: added solenoid support

--- bot_go_brr/src/initialize.rs
+++ bot_go_brr/src/initialize.rs
@@ -1,4 +1,10 @@
 //! Initialization routine for the robot
 
+use safe_vex::adi::{self, AdiConfig};
+use crate::config;
+
 /// The initialization routine entrypoint
-pub fn initialize() {}
+pub fn initialize() {
+    // configure the solenoid
+    adi::set_config(config::solenoid::PORT, AdiConfig::DigitalOut);
+}
--- bot_go_brr/src/opcontrol.rs
+++ bot_go_brr/src/opcontrol.rs
@@ -6,22 +6,26 @@ use crate::{bytecode, config, controls};
 /// The opcontrol routine entrypoint
 pub fn opcontrol() {
     // variables that get mutated
-    let mut now = rtos::millis();
-    let mut tick: u32 = 0;
+    let mut now = rtos::millis(); // the current time
+    let mut tick: u32 = 0; // the current tick
+    let mut solenoid_active = false; // if the solenoid is active or not
+    let mut solenoid_tick = tick; // the last time the solenoid's activity was changed
 
     // opcontrol loop
     loop {
-        cycle(&mut tick);
+        cycle(tick, &mut solenoid_active, &mut solenoid_tick);
         tick += 1;
         rtos::task_delay_until(&mut now, config::TICK_SPEED);
     }
 }
 
 /// An individual opcontrol cycle
-fn cycle(_tick: &mut u32) {
+fn cycle(tick: u32, solenoid_active: &mut bool, solenoid_tick: &mut u32) {
     // get belt instruction
     let belt_inst = controls::belt();
+    let solenoid_inst = controls::solenoid(tick, solenoid_active, solenoid_tick);
 
     // execute all generated instructions
     bytecode::execute(belt_inst);
+    bytecode::execute(solenoid_inst);
 }
