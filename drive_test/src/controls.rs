b9df5cdab65a9943ee7dde1f3a46e5a3de9f6245
kalscium <kalscium@protonmail.com>
2024-10-22 14:30:05 +1100

    Sim PID & State: added pid and state without modification of game code

--- drive_test/src/controls.rs
+++ drive_test/src/controls.rs
@@ -1,3 +1,4 @@
+use bevy::prelude::Resource;
 use rand::Rng;
 
 /// A macro for printing debug information
@@ -7,8 +8,23 @@ macro_rules! debug {
     };
 }
 
+/// State for the controls
+///
+/// Don't do this, this is just so that I can rapidly test new controls without modifing the game code
+#[derive(Resource)]
+pub struct ControlState {
+    pid: drive_controls::pid::Pid<f32>,
+}
+
+/// Initialises the control state
+pub fn init_state() -> ControlState {
+    ControlState {
+        pid: drive_controls::new_pid(),
+    }
+}
+
 /// Passes through a joystick's x and y values through all of the drive controls and returns the left and right drives and also any debug information
-pub fn controls(x: f32, y: f32, yaw: f32) -> (i32, i32, Vec<String>) {
+pub fn controls(x: f32, y: f32, yaw: f32, state: &mut ControlState) -> (i32, i32, Vec<String>) {
     let mut debug_info = Vec::new();
 
     // get the voltage values
@@ -16,7 +32,7 @@ pub fn controls(x: f32, y: f32, yaw: f32) -> (i32, i32, Vec<String>) {
     let yv = drive_controls::exp_daniel(y);
 
     // course correct
-    let (xvc, yvc) = drive_controls::course_correct(xv, yv, yaw);
+    let (xvc, yvc) = drive_controls::course_correct(xv, yv, yaw, &mut state.pid);
 
     // get the final left and right drive voltages
     let (ldr, rdr) = drive_controls::arcade(xvc as i32, yvc as i32);
@@ -39,7 +55,7 @@ pub fn controls(x: f32, y: f32, yaw: f32) -> (i32, i32, Vec<String>) {
 /// A function that controls the noise of the left and right drives of the robot (from -1..=1)
 pub fn noise(ldr: f32, rdr: f32) -> (f32, f32) {    
     // add random noise
-    let ldr = ldr + rand::thread_rng().gen_range(-100..100) as f32 * 0.01;
+    // let ldr = ldr + rand::thread_rng().gen_range(-100..100) as f32 * 0.01;
     let rdr = rdr + rand::thread_rng().gen_range(-100..100) as f32 * 0.01;
 
     (ldr, rdr)
