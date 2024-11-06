88a5bf47bce1457ee3f7a5935008c1d156829bc5
kalscium <kalscium@protonmail.com>
2024-11-06 17:23:12 +1100

    logic: inst: simplified inst to not have cycles and also added a iterator for iterating over autonomous routines

--- logic/src/inst.rs
+++ logic/src/inst.rs
@@ -2,16 +2,18 @@
 
 use packed_struct::prelude::*;
 
+use crate::{info, warn};
+
 /// The size (in bytes) of a single packed auton instruction
 ///
 /// Make sure to update the value in the Inst struct too
-pub const INST_SIZE: usize = 5;
+pub const INST_SIZE: usize = 4;
 
 /// A single autonomous intruction
 #[derive(Debug, Clone, Copy, PackedStruct)]
 #[packed_struct(bit_numbering="msb0")]
 #[packed_struct(endian="msb")]
-#[packed_struct(size_bytes=5)]
+#[packed_struct(size_bytes=4)]
 pub struct Inst {
     /// Required angle for the instruction to execute
     #[packed_field(bits="0..9")]
@@ -34,8 +36,48 @@ pub struct Inst {
     /// The 'thrust' *(exp y value)* of the robot
     #[packed_field(bit="12..27")]
     act_thrust: Integer<i16, packed_bits::Bits::<15>>,
+}
 
-    /// How many cycles this instruction lasts for
-    #[packed_field(bit="27..40")]
-    cycles: Integer<i16, packed_bits::Bits<13>>
+/// A stored autonomous routine
+pub struct AutonRoutine<'a> {
+    /// Internal reference to the packed instructions slice
+    insts: &'a [[u8; INST_SIZE]],
+    /// The current inst index into the inst slice
+    idx: usize,
+}
+
+impl<'a> AutonRoutine<'a> {
+    /// Creates a new autonomous routine iterator over a slice of packed insts
+    pub fn new(insts: &'a [[u8; INST_SIZE]]) -> Self {
+        Self {
+            insts,
+            idx: 0,
+        }
+    }
+}
+
+impl Iterator for AutonRoutine<'_> {
+    type Item = Inst;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        // get the packed inst
+        let Some(packed) =
+            self.insts.as_ref().get(self.idx)
+        else {
+            return None;
+        };
+
+        // try unpack the inst
+        let Ok(inst) =
+            Inst::unpack(packed)
+        else {
+            warn!("packed inst `{}` in auton routine at idx `{}` is invalid", hex::encode(packed), self.idx);
+            info!("skipping the rest of autonomous");
+            return None;
+        };
+
+        // update the index and return inst
+        self.idx += 1;
+        Some(inst)
+    }
 }
