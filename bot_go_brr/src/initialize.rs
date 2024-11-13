2cba5a2903a608f883eb44fee0eada906f130f4f
kalscium <kalscium@protonmail.com>
2024-11-13 17:01:30 +1100

    robot: odom calibration: added odom rotation sensor calibration

--- bot_go_brr/src/initialize.rs
+++ bot_go_brr/src/initialize.rs
@@ -1,7 +1,7 @@
 //! Initialization routine for the robot
 
 use logic::warn;
-use safe_vex::{adi::{self, AdiConfig}, imu};
+use safe_vex::{adi::{self, AdiConfig}, imu, rotation};
 use crate::config;
 
 /// The initialization routine entrypoint
@@ -13,5 +13,10 @@ pub fn initialize() {
     // calibrate the interial sensor
     if let Err(err) = imu::reset(config::IMU_PORT) {
         warn!("`PROSErr` occured while calibrating intertial sensor: {err:?}");
+    }
+
+    // calibrate the odom x rotation sensor
+    if let Err(err) = rotation::reset(config::odom::PORT_X) {
+        warn!("`PROSErr` occured while calibrating odom x rotation sensor: {err:?}");
     }
 }
