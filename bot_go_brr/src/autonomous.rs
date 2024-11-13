5b915803fa5db1a894a0662f74cf416b8107976f
kalscium <kalscium@protonmail.com>
2024-11-13 21:07:44 +1100

    robot: autonomous: updated autonomous playback to support the new odom requirements

--- bot_go_brr/src/autonomous.rs
+++ bot_go_brr/src/autonomous.rs
@@ -13,6 +13,10 @@ pub fn autonomous() {
     let mut now = rtos::millis(); // the current time
     let mut prev_vdr: (i32, i32) = (0, 0); // the previous voltages of the left and right drives
 
+    // variables for odometry
+    let mut prev_rot_y: f32 = 0.; // the previous measurement from the y rotation sensor
+    let mut y_coord: f32 = 0.; // the current calculated y coordinate of the robot
+
     // autonomous routine
     #[cfg(not(feature="full-autonomous"))]
     let auton_routine = AutonRoutine::new(&include!("autonomous/match_auton.rs")); // iterator over the auton routine insts
@@ -21,38 +25,33 @@ pub fn autonomous() {
 
     // autonomous loop
     for inst in auton_routine {
-        loop {
-            // get the current angle error
-            let angle_error = maths::absf(logic::drive::low_angle_diff(
-                i16::from(inst.req_angle) as f32,
-                drive::get_yaw(),
-            ));
+        // update odometry calculation
+        logic::odom::account_for(
+            drive::get_rotation_angle(config::auton::ODOM_Y_PORT),
+            &mut prev_rot_y,
+            &mut y_coord,
+        );
 
-            // if the absolute error is smaller than the angle precision requirement then run the inst's actions and advance to the next instruction
-            if angle_error <= config::auton::ANGLE_PRECISION {
-                // once it meets the requirements, then execute it's actions
-                belt::inst_control(inst.act_belt_active, inst.act_belt_up);
-                solenoid::inst_control(inst.act_solenoid_active);
+        // get the current angle error
+        let mut angle_error = logic::drive::low_angle_diff(
+            i16::from(inst.req_angle) as f32,
+            drive::get_yaw(),
+        );
 
-                // flush logs
-                info!("belt active: {}, belt up: {}", inst.act_belt_active, inst.act_belt_up);
-                info!("solenoid active: {}", inst.act_solenoid_active);
-                log::logic_flush(&mut logfile);
+        // get the current y coord error
+        let mut y_coord_error = i16::from(inst.req_odom_y) as f32 - y_coord;
 
-                // wait a tick cycle (also sort of an inst action)
-                rtos::task_delay_until(&mut now, config::TICK_SPEED);
-
-                break; // advance to the next inst
-            }
-
-            // correct for the required angle
-
-            // get the correction ldr and rdr (for the angle)
+        // keep correcting as long as the error is too large (outside of precision limit)
+        while
+            maths::absf(angle_error) > config::auton::ANGLE_PRECISION
+            || maths::absf(y_coord_error) > config::auton::ODOM_PRECISION
+        {
+            // correct for the errors
             let (ldr, rdr) = logic::drive::inst_control(
                 i16::from(inst.req_angle) as f32,
                 i16::from(inst.req_odom_y) as f32,
                 drive::get_yaw(),
-                todo!(),
+                y_coord,
                 &mut prev_vdr,
             );
 
@@ -65,6 +64,35 @@ pub fn autonomous() {
 
             // wait a tick cycle inbetween corrections
             rtos::task_delay_until(&mut now, config::TICK_SPEED);
+
+            // update odometry calculation
+            logic::odom::account_for(
+                drive::get_rotation_angle(config::auton::ODOM_Y_PORT),
+                &mut prev_rot_y,
+                &mut y_coord,
+            );
+
+            // update the errors
+            angle_error = logic::drive::low_angle_diff(
+                i16::from(inst.req_angle) as f32,
+                drive::get_yaw(),
+            );
+            y_coord_error = i16::from(inst.req_odom_y) as f32 - y_coord;
         }
+
+        // once it meets the requirements, then execute it's actions
+        belt::inst_control(inst.act_belt_active, inst.act_belt_up);
+        solenoid::inst_control(inst.act_solenoid_active);
+
+        // logs
+        info!("precision requirements achieved");
+        info!("belt active: {}, belt up: {}", inst.act_belt_active, inst.act_belt_up);
+        info!("solenoid active: {}", inst.act_solenoid_active);
+
+        // flush logs
+        log::logic_flush(&mut logfile);
+
+        // wait a tick cycle
+        rtos::task_delay_until(&mut now, config::TICK_SPEED);
     }
 }
