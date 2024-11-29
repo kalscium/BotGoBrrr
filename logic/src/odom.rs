053a34b7b94ce40ed857a85fa9c8b7ebde29697d
kalscium <kalscium@protonmail.com>
2024-11-29 16:16:31 +1100

    logic: odom: improved odom through the usage of two instead of one rotation sensors for each axis

--- logic/src/odom.rs
+++ logic/src/odom.rs
@@ -5,12 +5,12 @@
 use core::f32::consts::PI;
 
 /// The diameter of the robot's wheel in **mm**
-const DIAMETER: f32 = 69.85;
+const DIAMETER: f32 = 101.6;
 
 /// Finds the difference in rotation of the rotation sensors (in degrees)
-pub fn lowest_rot_delta(x: f32, y: f32) -> f32 {
+pub fn lowest_rot_delta(prev: f32, measurement: f32) -> f32 {
     // get the first possible difference in angle
-    let delta1 = y - x;
+    let delta1 = measurement - prev;
 
     // get the second possible difference in angle
     let delta2 = delta1 - 360. * maths::signumf(delta1);
@@ -23,18 +23,47 @@ pub fn lowest_rot_delta(x: f32, y: f32) -> f32 {
     }
 }
 
-/// Modifies an absolute coordinate based upon the current and previous angle of the rotation sensor
-pub fn account_for(current: f32, prev: &mut f32, coord: &mut f32) {
+#[derive(Debug, Clone)]
+pub struct OdomState {
+    /// The previous left rotational sensor for measuring the y coordinate
+    prev_ly: f32,
+    /// The previous right rotational sensor for measuring the y coordinate
+    prev_ry: f32,
+    /// The current y-coordinate
+    pub y_coord: f32,
+}
+
+/// Approximates the distance travelled from the previous and current angle of the rotational sensor
+pub fn distance(current: f32, prev: &mut f32) -> f32 {
     // find the delta
     let angle_delta = lowest_rot_delta(*prev, current);
 
-    // update the previous angle to the current one
+    // update the previous angle
     *prev = current;
 
-    // find the coordinate delta though the circumference of the wheel
-    const CIRCUMFERENCE: f32 = DIAMETER*PI; // circumference in millimeters
-    let coord_delta = angle_delta/360. * CIRCUMFERENCE;
+    // calculate the distance travelled from the angle
+    const CIRCUMFERENCE: f32 = DIAMETER * PI; // circumference in mm
+    let coord_delta = angle_delta / 360. * CIRCUMFERENCE;
 
-    // add the coordinate delta to the coordinate
-    *coord += coord_delta;
+    // return it
+    coord_delta
+}
+
+/// Modifies an absolute coordinate based upon the current and previous angle of the rotation sensors
+pub fn account_for(
+    current_ly: f32,
+    current_ry: f32,
+    state: &mut OdomState,
+) {
+    // find the coordinate delta for the left y rotation sensor
+    let ly_delta = distance(current_ly, &mut state.prev_ly);
+
+    // find the coordinate delta for the right y rotation sensor
+    let ry_delta = distance(current_ry, &mut state.prev_ry);
+
+    // average the deltas for the y coord
+    let y_delta = maths::avgf(ly_delta, ry_delta);
+
+    // add the coordinate deltas to the state
+    state.y_coord += y_delta;
 }
