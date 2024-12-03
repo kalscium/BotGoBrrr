3a6475ac20c40049f303e577973f132f5aa1194d
kalscium <kalscium@protonmail.com>
2024-12-04 10:53:55 +1100

    more tests: added more test function calls in asm
    Testing more possible function calls to see if they work or not in asm.

--- src/opcontrol.s
+++ src/opcontrol.s
@@ -2,10 +2,26 @@
 
 .text
 opcontrol:
-	ldr r0, =msg
+	@ push to the stack
+	push {lr}
+
+	@ print hello world
+	ldr r0, =hw
 	bl printf
+
+	@ print a 'random' number
+	ldr r0, =random
+	mov r1, #7
+	bl printf
+
+	@ spins a motor at 12000 rpm
+	mov r0, #1
+	mov r1, #12000
+	bl motor_move_voltage
 	
+	pop {lr}
 	bx lr @ return to to the c function
 
 .data
-msg: .asciz "hello, world!"
+hw: .asciz "hello, world!"
+random: .asciz "random number: "
