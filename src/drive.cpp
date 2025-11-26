4d755e975c8caddf6d1c7163bbd5515adb0784ab
kalscium <kalscium@protonmail.com>
2025-11-26 14:58:12 +1100

    Project Init
    I've initialized the project.
    Basically template code with enough to arcade drive somewhat.
    
    Signed-off-by: Ethan Ma <kalscium@protonmail.com>

---
+++ src/drive.cpp
@@ -0,0 +1,16 @@
+// Drivetrian stuff
+
+#include "drive.hpp"
+#include "api.h"
+
+// Left Drivetrain Motors
+pros::MotorGroup left_dt({ 10, 9, -14 });
+// Right Drivetrain Motors
+pros::MotorGroup right_dt({ -11, -2, 12 });
+
+void initDt() {
+}
+
+void driverDrive() {
+        
+}
---
+++ src/drive.hpp
@@ -0,0 +1,8 @@
+#pragma once
+#include "api.h"
+
+// Initializes the drivetrain
+void initDt();
+
+// Driver drive controls
+void driverDrive();
